9be55bf77d8bd180a58cec429c2fee738751e3f9
SeLigaNoNick <lucasmw15@live.com>
2014-11-22 12:54:54 -0200

    colocando metodos

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <title>Criador de Classes Automatico</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
         <script type="text/javascript" src="js/java.js"></script>
@@ -12,29 +12,26 @@
     <body>
         <h1>Criador de Classes Automatico</h1>
 
-        <h2>GET SET</h2>
         <form action="" method="POST">
-            <div class="form">
-                <div class="ajuste">
-                <label for="nomeatributo">Nome do Atributo: </label>
-                <input type="text" class="form-control" name="nomeatributo[]" value="">
-                </div>
-                <div class="ajuste">
-                <label for="variavel">Nome da Variavel: </label>
-                <input type="text" class="form-control" name="variavel[]">
-                </div>
-                <div class="ajuste">
+            <div class="ajuste1">
                 <label for="classe">Nome da Classe: </label>
-                <input type="text" class="form-control" name="classname">
-                </div>
+                <input type="text" class="form-control" name="classname" required>
             </div>
-            <div id="oculto"></div>
-            <div id="enviar">
-                <input type="submit" class="btn btn-danger" name="enviar" value="enviar">
+
+            <h2>GET SET</h2>
+
+            <div id="getset"></div>
+
+            <div class="enviar">
                 <input id="adicionar" type="button" class="btn btn-danger" value="Adicionar" onclick="javasript:Adicionar()">
             </div>
 
-            <div id="oculto1"></div>
+            <h2>Métodos</h2>
+
+            <div id="metodos">
+                <input id="metodo" type="button" class="btn btn-danger" value="Adicionar Metodos" onclick="javasript:Update()">
+            </div>
+            <input id="enviar" type="submit" class="btn btn-danger" name="enviar" value="enviar">
         </form>
         <?php
         if (isset($_POST["enviar"]))
@@ -45,11 +42,23 @@
             $strPrivates = "";
             $strcons = "";
             $strcomp = "";
+            $strMetodo = "";
 
             //Recebendo valores do formulario
             $classname = $_POST["classname"];
-            $atributo = $_POST["nomeatributo"];
-            $variavel = $_POST["variavel"];
+
+            if (isset($_POST["nomeatributo"])) {
+                $atributo = $_POST["nomeatributo"];
+            }
+
+            if (isset($_POST["variavel"])) {
+                $variavel = $_POST["variavel"];
+            }
+
+            if (isset($_POST["metodo"])) {
+                $metodo = $_POST["metodo"];
+            }
+            
             $classname2 = $classname . '.php';
             
             //Verificando se o arquivo existe
@@ -57,7 +66,9 @@
                 //Pegando o conteudo do arquivo
                 $strFile = file_get_contents($classname2);
                 //Recebendo os valores da VARIAVEL e as separando
-                $strvar = '$' . implode(", $", $variavel).',';
+                if (isset($_POST["variavel"])) {
+                    $strvar = '$' . implode(", $", $variavel).',';
+                }
             }else{
                 //Fazendo a estrutura do arquivo
                 $strFile .= '<?php'."\r\n";
@@ -65,29 +76,48 @@
                 $strFile .= "    \n\r";
                 $strFile .= '    public function __construct($'.implode(", $", $variavel).'){' . "\r\n";
                 $strFile .= "    }". "\r\n";
-                $strFile .= "\r\n    //SET GET";
+                $strFile .= "\r\n    //SET GET\r\n";
+                $strFile .= "    //Metodos\r\n";
                 $strFile .= "}";
             }
             
             //Retirando os valores das variaveis e as estruturando
-            foreach ($atributo as $key => $value) 
+            if (isset($_POST["nomeatributo"]))
             {
-                $strPrivates .= "\r\n    private $$value;";
-                $strcons.= "\r\n        " . '$this->set' . $value .'($' . $variavel[$key] . ');';
-                $strcomp .= "\r\n" . '    public function set'.$value.'($'. $variavel[$key] . '){'."\r\n";
-                $strcomp .= "        " . '$this->' . $value . ' = $' . $variavel[$key].';'."\r\n";
-                $strcomp .= '    }'."\r\n";
-                $strcomp .= "\r\n".'    public function get'.$value.'(){'."\r\n";
-                $strcomp .= '        return $this->'.$value.';'."\r\n";
-                $strcomp .= '    }'."\r\n";
+                foreach ($atributo as $key => $value) 
+                {
+                    $strPrivates .= "\r\n    private $$value;";
+                    $strcons.= "\r\n        " . '$this->set' . $value .'($' . $variavel[$key] . ');';
+                    $strcomp .= "\r\n" . '    public function set'.$value.'($'. $variavel[$key] . '){'."\r\n";
+                    $strcomp .= "        " . '$this->' . $value . ' = $' . $variavel[$key].';'."\r\n";
+                    $strcomp .= '    }'."\r\n";
+                    $strcomp .= "\r\n".'    public function get'.$value.'(){'."\r\n";
+                    $strcomp .= '        return $this->'.$value.';'."\r\n";
+                    $strcomp .= '    }'."\r\n";
+                }
+                $strFile = preg_replace("/(public function __construct\(.*\{)/i", "$1 $strcons", $strFile);
+                $strFile = preg_replace("/(public function __construct\()/i", "$1 $strvar", $strFile);
+                $strFile = preg_replace("/(\/\/SET GET)/i", "$1 $strcomp", $strFile);
             }
+
+            if (isset($_POST["metodo"])) 
+            {
+                foreach ($metodo as $key => $value) 
+                {
+                    foreach ($value["metodo"] as $key1 => $value1) 
+                    {
+                        $strMetodo .= "\r\n"."    public function ". $value1."($".implode(", $", $value["atributo"])."){\r\n";
+                        $strMetodo .= "\r\n";
+                        $strMetodo .= "    }\r\n";
+                    }
+                }
+                $strFile = preg_replace("/(\/\/Metodos)/i", "$1 $strMetodo", $strFile);
+            }
+
 
             //Definindo o local onde os dados serao escritos
             $strFile = preg_replace("/(class +.*\{)/i", "$1 $strPrivates", $strFile);
-            $strFile = preg_replace("/(public function __construct\(.*\{)/i", "$1 $strcons", $strFile);
-            $strFile = preg_replace("/(public function __construct\()/i", "$1 $strvar", $strFile);
-            $strFile = preg_replace("/(\/\/SET GET)/i", "$1 $strcomp", $strFile);
-
+            
             //Escrevendo os dados
             file_put_contents($classname2, $strFile);
         }
--- js/java.js
+++ js/java.js
@@ -1,57 +1,114 @@
-var numSG = 0;
-var numNovo = 0;
-var numAtributo = 0;
-$(document).readyfunction()
+var numArray = 0;
+function Adicionar()
 {
-    function Adicionar()
-    {
-        numSG++;
-        var copia = "";
-        var copia = '<div class="copia copia'+numSG+'">'+'<div class="ajuste"><label for="nomeatributo">Nome do Atributo: </label><input type="text" class="form-control" name="nomeatributo[]" value=""></div><div class="ajuste"><label for="variavel">Nome da Variavel: </label><input type="text" class="form-control" name="variavel[]"></div></div>';
-        $(copia).clone().insertBefore("#oculto");
+    var copia = "";
+    var copia = '<div class="copia">'+
+                    '<div class="ajuste">'+
+                    '<label for="nomeatributo">Nome do Atributo: </label>'+
+                    '<input type="text" class="form-control" name="nomeatributo[]" value="">'+
+                    '</div>'+
+                    '<div class="ajuste">'+
+                    '<label for="variavel">Nome da Variavel: </label>'+
+                    '<input type="text" class="form-control" name="variavel[]">'+
+                    '</div>'+
+                '</div>';
+    $("#getset").append(copia);
 
-        if (numSG == 1) {
-            var deletar = "";
-            var deletar = '<input id="deletar" type="button" onclick="javasript:Fechar()" class="btn btn-danger"value="Deletar">';
-            $(deletar).clone().insertAfter("#adicionar");
-        };
-    }
-
-    function Update()
-    {
-        $("#metodo").remove();
-        var metodo = "";
-        var metodo = '<h2>Métodos</h2><div class="metodo'+numNovo+'"><label for="NomeMetodo">Nome do Método:</label><input type="text" class="form-control" name="metodo[]"><label for="Atributo">Nome do Atributo</label><input type="text" class="form-control atributo'+numNovo+'" name="atributometodo[]"></div><div class="ajustemetodo"><input id="metodo" type="button" class="btn btn-danger" value="Adicionar Metodos" onclick="javasript:AdicionarMetodo()"><input type="button" class="btn btn-danger" value="Adicionar Atributo" onclick="javasript:AdicionarAtributo()"></div>';
-        $(metodo).clone().insertBefore("#oculto1");
-    }
-
-    function AdicionarMetodo()
-    {
-        numNovo++;
-        var metodos = "";
-        var metodos = '<div class="metodo'+numNovo+'"><label for="NomeMetodo">Nome do Método:</label><input type="text" class="form-control" name="metodo[][]"><label for="Atributo">Nome do Atributo</label><input type="text" class="form-control atributo'+numNovo+'" name="atributometodo[]"></div>';
-        $(metodos).clone().insertBefore(".ajustemetodo");
-    }
-
-    function AdicionarAtributo()
-    {
-        numAtributo++;
-        var atributo = "";
-        var atributo = '<label for="Atributo">Nome do Atributo</label><input type="text" class="form-control atributo'+numAtributo+'" name="atributometodo[]">';
-        $(atributo).clone().insertAfter(".atributo"+numNovo);
-    }
+    if ($(".copia").length == 1) {
+        var btnDeletar = "";
+        var btnDeletar = '<input id="deletar" type="button" onclick="javasript:Fechar()" class="btn btn-danger"value="Deletar">';
+        $(btnDeletar).clone().insertAfter("#adicionar");
+    };
 };
+
+function Update()
+{
+    $("#metodo").remove();
+    var metodo = "";
+    var metodo = '<div id="Bmetodo">'+
+        '<div class="col-md-4 metodo">'+
+            '<div class="ajuste2">'+
+            '<label for="NomeMetodo">Nome do Método:</label>'+
+            '<input type="text" class="form-control" name="metodo['+numArray+'][metodo][]" required>'+
+            '</div>'+
+            '<div class="ajuste3">'+
+            '<label for="Atributo">Nome do Atributo</label>'+
+            '<input type="text" class="form-control atributo" name="metodo['+numArray+'][atributo][]">'+
+            '</div>'+
+        '</div></div>';
+    var botao = "";
+    var botao = 
+            '<div class="ajustemetodo">'+
+              '<input id="metodo2" type="button" class="btn btn-danger" value="Adicionar Metodos" onclick="javasript:AdicionarMetodo()">'+
+              '<input type="button" class="btn btn-danger" value="Adicionar Atributo" onclick="javasript:AdicionarAtributo()">'+
+            '</div>'+
+            '<div class="ajustemetodo">'+
+            '<input id="metodo" type="button" class="btn btn-danger" value="Remover Metodo" onclick="javascript:FecharMetodo()">'+
+              '<input id="atributo" type="button" class="btn btn-danger" value="Remover Atributo" onclick="javascript:FecharAtributo()">'+
+            '</div>';
+    $("#metodos").append(metodo);
+    $(botao).insertAfter("#metodos:last");
+}
+
+function AdicionarMetodo()
+{
+    numArray++; 
+    var metodos = "";
+    var metodos = '<div class="col-md-4 metodo">'+
+            '<div class="ajuste2">'+
+            '<label for="NomeMetodo">Nome do Método:</label>'+
+            '<input type="text" class="form-control" name="metodo['+numArray+'][metodo][]" required>'+
+            '</div>'+
+            '<div class="ajuste3">'+
+            '<label for="Atributo">Nome do Atributo</label>'+
+            '<input type="text" class="form-control atributo" name="metodo['+numArray+'][atributo][]">'+
+            '</div>'+
+        '</div>';
+    $(metodos).clone().insertAfter(".metodo:last");
+}
+
+function AdicionarAtributo()
+{
+    var atributo = "";
+    var atributo = '<div class="ajuste3">'+
+            '<label for="Atributo">Nome do Atributo</label>'+
+            '<input type="text" class="form-control atributo" name="metodo['+numArray+'][atributo][]">'+
+            '</div>';
+    $(atributo).insertAfter(".ajuste3:last");
+}
 
 function Fechar()
 {
-    if (".copia"+numSG) {
-        $(".copia"+numSG).remove();
-        numSG--;
-    }else{
-        $(".copia"+numSG).remove();
-    };  
+    if($(".copia").length) {
+        $(".copia").last().remove();
+    };
 
-    if (numSG == 0) {
+    if($(".copia").length == 0){
         $("#deletar").remove();
     };
+}
+
+function FecharMetodo()
+{
+
+    if($(".metodo").length) {
+        $(".metodo").last().remove();
+    };
+
+    if($(".metodo").length == 0){
+        $("#Bmetodo").remove();
+        $(".ajustemetodo").remove();
+        var botao = '<input id="metodo" type="button" class="btn btn-danger" value="Adicionar Metodos" onclick="javasript:Update()">';
+        $("#metodos").append(botao);
+    };
+    numArray--;
+}
+
+function FecharAtributo()
+{
+    if ($(".ajuste2").length == $(".ajuste3").length) {
+
+    }else{
+        $(".ajuste3").last().remove();
+    }
 }
